885ed05a053fc8efc541cd368c7900e86397f94a
Hakan İSMAİL <hakanismail53@gmail.com>
2023-12-04 18:10:08 +0300

    Files Updated

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Easy Effects SBC Configs
  Sound blaster configs for Easy Effects
+
+ **These files were made by extracting the settings in Sound blaster cinema 5, 6 and 6+ applications.**
+
+A collection of presets for [wwmm's EasyEffects](https://github.com/wwmm/EasyEffects)
+
+It currently contains:
+
+1. **Perfect EQ**
+
+   This preset only enables [Ziyad Nazem's __"Perfect EQ"__](https://www.ziyadnazem.com/post/956431457/the-perfect-eq-settings-unmasking-the-eq)
+
+
+# Auto Installation
+The `install.sh` script will try to install the presets automatically, just fire up a terminal and run the command:
+```shell
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Hakanbaban53/Easy-Effects-SBC-Configs/master/install.sh)"
+```
+**NOTE**: the script requires `curl` to be installed. Ubuntu doesn't ship with curl installed by default, so you have to first run
+
+```Ubuntu/Debian
+sudo apt install curl
+```
+```Arch
+sudo pacman -S curl
+```
+before running the script.
+
+# Manual installation
+Just clone the repository and then copy the `.json` files into the `EasyEffects/output` directory which you can find in the local `config` directory. Obviously the location of that directory depends on how you installed EasyEffects. If you installed it through Flatpak, you can find it in `~/.var/app/com.github.wwmm.easyeffects/config/easyeffects`, or if you used the PPA for Ubuntu (or the AUR package for Arch) it should be `~/.config/easyeffects`
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # This script automatically detect the EasyEffects presets directory and installs the presets
 
-IRLGIT_REPOSITORY="https://raw.githubusercontent.com/JackHack96/EasyEffects-Presets/master"
 SBCGIT_REPOSITORY="https://raw.githubusercontent.com/Hakanbaban53/Easy-Effects-SBC-Configs"
 
 check_installation() {
@@ -16,11 +15,6 @@ check_installation() {
     mkdir -p $PRESETS_DIRECTORY
 }
 
-check_impulse_response_directory() {
-    if [ ! -d "$PRESETS_DIRECTORY/irs" ]; then
-        mkdir "$PRESETS_DIRECTORY/irs"
-    fi
-}
 
 read_choice() {
     while :; do
@@ -45,23 +39,6 @@ install_presets() {
     1)
         echo "Installing Bass Enhancing + Perfect EQ preset..."
         curl "$IRLGIT_REPOSITORY/Bass%20Enhancing%20%2B%20Perfect%20EQ.json" --output "$PRESETS_DIRECTORY/output/Bass Enhancing + Perfect EQ.json" --silent
-        echo "Installing impulse response files..."
-        curl "$IRLGIT_REPOSITORY/irs/Dolby%20ATMOS%20((128K%20MP3))%201.Default.irs" --output "$PRESETS_DIRECTORY/irs/Dolby ATMOS ((128K MP3)) 1.Default.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/MaxxAudio%20Pro%20((128K%20MP3))%204.Music%20w%20MaxxSpace.irs" --output "$PRESETS_DIRECTORY/irs/MaxxAudio Pro ((128K MP3)) 4.Music w MaxxSpace.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Razor%20Surround%20((48k%20Z-Edition))%202.Stereo%20+20%20bass.irs" --output "$PRESETS_DIRECTORY/irs/Razor Surround ((48k Z-Edition)) 2.Stereo +20 bass.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20Earpods%20HIFI.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) Earpods HIFI.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20MDR-E9LP%20HIFI.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) MDR-E9LP HIFI.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20MDR-E9LP%20SM%20SRH940.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) MDR-E9LP SM SRH940.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20MDR-E9LP%20SM%20XBA3.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) MDR-E9LP SM XBA3.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20MDR-E9LP%20SM%20beyerT1.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) MDR-E9LP SM beyerT1.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20MDR-XB500%20HIFI.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) MDR-XB500 HIFI.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20XBA-H3%20HIFI.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) XBA-H3 HIFI.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20XBA-H3%20SM%20SRH940.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) XBA-H3 SM SRH940.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20XBA-H3%20SM%20XBA4.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) XBA-H3 SM XBA4.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Accudio%20((48kHz%20Z.E.))%20XBA-H3%20SM%20beyerT1.irs" --output "$PRESETS_DIRECTORY/irs/Accudio ((48kHz Z.E.)) XBA-H3 SM beyerT1.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Creative%20X-Fi%20((Z-Edition))%20Crystalizer%2010%20%2B%20Expand%2010.irs" --output "$PRESETS_DIRECTORY/irs/Creative X-Fi ((Z-Edition)) Crystalizer 10 + Expand 10.irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/HTC%20Beats%20Audio%20((Z-Edition)).irs" --output "$PRESETS_DIRECTORY/irs/HTC Beats Audio ((Z-Edition)).irs" --silent
-        curl "$IRLGIT_REPOSITORY/irs/Waves%20MaxxAudio%20((Z-Edition))%20AudioWizard%201.Music.irs" --output "$PRESETS_DIRECTORY/irs/Waves MaxxAudio ((Z-Edition)) AudioWizard 1.Music.irs" --silent
         echo "Installing SBC Gaming Present..."
         curl "$SBCGIT_REPOSITORY/Gaming.json" --output "$PRESETS_DIRECTORY/output/Gaming.json" --silent
         echo "Installing SBC Music Present..."
@@ -75,7 +52,6 @@ install_presets() {
 }
 
 check_installation
-check_impulse_response_directory
 install_menu
 read_choice
 install_presets
